8093fa347765c0678ba934d19ebdee03801e9875
wenlong_shaw <xwl0012@126.com>
2021-04-16 09:59:53 +0800

    rob

--- rob.go
+++ rob.go
@@ -7,6 +7,23 @@ package leetcode
 // 	return b
 // }
 
+func robOnce(nums []int) int {
+	n := len(nums)
+	if n == 0 {
+		return 0
+	}
+	if n == 1 {
+		return nums[0]
+	}
+	dp := make([]int, 2)
+	dp[0] = nums[0]
+	dp[1] = max(nums[0], nums[1])
+	for i := 2; i < n; i++ {
+		dp[i%2] = max(nums[i]+dp[(i-2)%2], dp[(i-1)%2])
+	}
+	return dp[(n-1)%2]
+}
+
 func rob(nums []int) int {
 	n := len(nums)
 	if n == 0 {
@@ -15,11 +32,5 @@ func rob(nums []int) int {
 	if n == 1 {
 		return nums[0]
 	}
-	dp := make([]int, n)
-	dp[0] = nums[0]
-	dp[1] = max(nums[0], nums[1])
-	for i := 2; i < n; i++ {
-		dp[i] = max(nums[i]+dp[i-2], dp[i-1])
-	}
-	return dp[n-1]
+	return max(robOnce(nums[0:n-2]), robOnce(nums[1:n-1]))
 }
